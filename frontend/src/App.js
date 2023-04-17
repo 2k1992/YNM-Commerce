80189a97366b34288eab39152546b0eae294a76c
2k1992 <kenyeliab@gmail.com>
2023-04-17 12:50:54 +0300

    simple landing page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
+import { Button } from 'react-bootstrap';
+import { CartFill } from 'react-bootstrap-icons';
 import logo from './assets/image/logo.png';
+import data from './data';
 function App() {
   return (
     <div>
@@ -8,7 +11,27 @@ function App() {
         </a>
       </header>
       <main>
-        <h1> Products</h1>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}ETB</strong>
+                </p>
+                <Button variant="primary">
+                  <CartFill /> Add to Cart
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
