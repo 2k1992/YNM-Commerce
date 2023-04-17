acc0070106aef19fe49601a77d497a270188420c
2k1992 <kenyeliab@gmail.com>
2023-04-17 20:20:47 +0300

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,27 @@
-import { Button } from 'react-bootstrap';
-import { CartFill } from 'react-bootstrap-icons';
 import logo from './assets/image/logo.png';
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">
-          <img alt="logo" src={logo} />
-        </a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>{product.price}ETB</strong>
-                </p>
-                <Button variant="primary">
-                  <CartFill /> Add to Cart
-                </Button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          {/* we use "Link to" so that the page stays single page 
+          (so it doesn't refresh) when routing between contents */}
+          <Link to="/">
+            <img alt="logo" src={logo} />
+          </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
