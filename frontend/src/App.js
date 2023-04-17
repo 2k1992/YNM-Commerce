d2069cbef1e5ec33cc947f4039282664e82dc35f
2k1992 <kenyeliab@gmail.com>
2023-04-17 10:09:02 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './assets/image/logo.png';
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">
+          <img alt="logo" src={logo} />
+        </a>
+      </header>
+      <main>
+        <h1> Products</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
