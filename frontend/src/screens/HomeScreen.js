e844a8b863054d8ea10970dc771861a9c54cfc51
2k1992 <kenyeliab@gmail.com>
2023-04-17 21:47:40 +0300

    create node server and fetch products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,28 @@
 import { Button } from 'react-bootstrap';
-import data from '../data';
+// import data from '../data';
 import { CartFill } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function HomeScreen() {
+  // useState is a react hook
+  // we'll use it to return a variable (products) and a function(setProducts)
+  // to update this variable
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data); // result.data is the products in the backend
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
         {/* to get the data from data.js and display it  */}
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
