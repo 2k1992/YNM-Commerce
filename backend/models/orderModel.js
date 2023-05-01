df51f599f0a2bfd6d23f7c404a277a838cecde87
2k1992 <kenyeliab@gmail.com>
2023-05-01 16:56:42 +0300

    chapa payment

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -22,6 +22,7 @@ const orderSchema = new mongoose.Schema(
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
+      email: { type: String, required: true },
     },
     paymentMethod: { type: String, required: true },
     paymentResult: {
